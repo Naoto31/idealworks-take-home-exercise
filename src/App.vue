785ed06df36cb36f54afa07b9a711c24581a89fa
Naoto31 <naoto0910sai@gmail.com>
2023-12-14 17:54:02 +0100

    create page folder

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HomePage from "./components/HomePage.vue";
+import HomePage from "./pages/HomePage.vue";
 
 export default defineComponent({
   name: "App",
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,5 +1,5 @@
 import { shallowMount } from "@vue/test-utils";
-import HomePage from "@/components/HomePage.vue";
+import HomePage from "@/pages/HomePage.vue";
 
 describe("HomePage.vue", () => {
   it("renders props.msg when passed", () => {
