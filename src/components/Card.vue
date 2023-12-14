e6468d31f135b559e6c7da7ab51107d329284c50
Naoto31 <naoto0910sai@gmail.com>
2023-12-14 21:07:23 +0100

    wip: home and comment card

---
+++ src/components/Card.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :style="{ marginLeft: `${nestedLevel * 20}px` }" class="card">
+    <img :src="avatar" alt="User avatar" class="avatar" />
+    <div class="content">
+      <h3>{{ username }}</h3>
+      <p>{{ message }}</p>
+      <span>{{ createdAt }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "CommentCard",
+  props: {
+    avatar: String,
+    username: String,
+    message: String,
+    createdAt: [String, Date],
+    nestedLevel: {
+      type: Number,
+      default: 0,
+    },
+  },
+};
+</script>
+
+<style>
+.card {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 10px 0;
+}
+.avatar {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,124 +1,57 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
-          target="_blank"
-          rel="noopener"
-          >unit-jest</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div>
+    <h1>Comments</h1>
+    <CommentCard></CommentCard>
+    <!-- <div v-for="post in topLevelPosts" :key="post.id">
+      <Card
+        :avatar="getUserAvatar(post.userId)"
+        :username="getUserName(post.userId)"
+        :message="post.message"
+        :createdAt="post.createdAt"
+        :nestedLevel="post.nestedLevel"
+      />
+      <div v-for="reply in post.replies" :key="reply.id">
+        <Card
+          :avatar="getUserAvatar(reply.userId)"
+          :username="getUserName(reply.userId)"
+          :message="reply.message"
+          :createdAt="reply.createdAt"
+          :nestedLevel="reply.nestedLevel"
+        />
+      </div>
+    </div> -->
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import CommentCard from "../components/Card.vue";
+import data from "../data.json";
+import { Post, PostUI } from "@/type";
 
-export default defineComponent({
+export default {
   name: "HomePage",
-  props: {
-    msg: String,
+  components: {
+    CommentCard,
   },
-});
+  setup() {
+    console.log(data);
+    // const topLevelPosts = buildCommentTree(data.comments);
+    // function getUserAvatar(userId: string) {
+    //   return data.users.find(user => user.id === userId)?.avatar || "";
+    // }
+    // function getUserName(userId: string) {
+    //   return data.users.find(user => user.id === userId)?.name || "Unknown";
+    // }
+    // function buildCommentTree(posts: PostUI[], parentId = null, level = 0) {
+    //   return posts
+    //     .filter(post => post.parentId === parentId)
+    //     .map(post => ({
+    //       ...post,
+    //       nestedLevel: level,
+    //       replies: buildCommentTree(posts, post.id, level + 1),
+    //     }));
+    // }
+    // return { topLevelPosts, getUserAvatar, getUserName };
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
