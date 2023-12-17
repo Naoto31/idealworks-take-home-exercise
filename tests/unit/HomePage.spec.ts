980021bd8a0927986b0f05786ef10de97ff4239a
Naoto31 <naoto0910sai@gmail.com>
2023-12-17 20:05:16 +0100

    update testing

--- tests/unit/HomePage.spec.ts
+++ tests/unit/HomePage.spec.ts
@@ -2,12 +2,23 @@ import { mount } from "@vue/test-utils";
 import HomePage from "@/pages/HomePage.vue";
 import CommentCard from "@/components/CommentCard.vue";
 import AddCommentCard from "@/components/AddCommentCard.vue";
+import { createPinia, setActivePinia } from "pinia";
 import data from "@/data.json";
 
 describe("HomePage.vue", () => {
   let wrapper: any;
 
   beforeEach(() => {
+    // clear localStorage before each test
+    window.localStorage.clear();
+
+    // mock the localStorage with initial data
+    window.localStorage.setItem("comments", JSON.stringify(data.comments));
+
+    // Create and set an active Pinia instance
+    const pinia = createPinia();
+    setActivePinia(pinia);
+
     wrapper = mount(HomePage, {
       props: {},
       global: {
@@ -21,8 +32,6 @@ describe("HomePage.vue", () => {
 
   it("renders all the comments", () => {
     const commentCards = wrapper.findAllComponents(CommentCard);
-    console.log(commentCards.length);
-    console.log(data.comments.length);
     expect(commentCards.length).toBe(data.comments.length);
   });
 });
