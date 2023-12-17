22c81ab19ac79f563c9309fa24beb54f74a6b583
Naoto31 <naoto0910sai@gmail.com>
2023-12-17 17:11:51 +0100

    set users in store

--- src/store/user.ts
+++ src/store/user.ts
@@ -4,10 +4,14 @@ import { User } from "../type";
 export const useUserStore = defineStore("user", {
   state: () => ({
     currentUser: null as User | null,
+    users: null as User[] | null,
   }),
   actions: {
     setCurrentUser(user: User) {
       this.currentUser = user;
+    },
+    setUsers(users: User[]) {
+      this.users = users;
     },
   },
 });
